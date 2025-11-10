29a7bc03bffa00efa736ea40e7003c5531c7caca
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2025-11-10 20:37:08 -0300

    build: Criando projeto

---
+++ src/main/java/com/api/GS/GsApplication.java
@@ -0,0 +1,13 @@
+package com.api.GS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsApplication.class, args);
+	}
+
+}
